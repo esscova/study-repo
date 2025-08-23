d885b4fd0da17424932ea1199f26bf8eeb98915e
wellington moreira <wsantos08@hotmail.com>
2025-08-23 19:21:50 -0300

    refact: removendo diretorio java atual para substituição

--- Java/exercicios/ExerciciosJava/src/exercicios/Exercicios.java
+++ Java/exercicios/ExerciciosJava/src/exercicios/Exercicios.java
@@ -7,6 +7,10 @@ public class Exercicios {
 
     
     public static void main(String[] args) {
+        System.out.println("=== EXERCICIOS DE NIVELAMENTO ===");
+        System.out.println("""
+                           Exercicio: Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+                           mensagem explicativa.""");
         Exercicio001.executar();
     }
     
